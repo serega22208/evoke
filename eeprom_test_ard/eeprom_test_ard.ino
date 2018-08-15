934408272ccee600c0f1c34bec82d039140fd3c3
sergei <sergega@me.com>
2018-08-15 14:39:00 +0800

    lass

--- eeprom_test_ard/eeprom_test_ard.ino
+++ eeprom_test_ard/eeprom_test_ard.ino
@@ -1,15 +1,16 @@
 #include <Wire.h>
-#include <Crc16.h>
+
 
 #define ADDRESS 0x50
 
 //Crc 16 library (XModem)
-Crc16 crc; 
+//Crc16 crc;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-
+  Serial.println("CRC-16 bit test program");
+  Serial.println("=======================");
   Wire.begin();
 
 }
@@ -34,7 +35,7 @@ void read_large_eeprom(int address, unsigned long first, unsigned long bytes) {
       Serial.write("ERRoR");
       break;
     }
-Serial.write(address);
+//Serial.write(address);
     Wire.beginTransmission(address);
     ////starting transmitting over i2c with current adress
     ////После него последовательность байтов для передачи ставится
@@ -54,19 +55,34 @@ Serial.write(address);
     //read 1 byte from i2c eeprom and encode it to send over rs-232
     if (Wire.available()) {
       value = Wire.read();
-      //The packet format is as follows:
-      //@@Header(0x06,0x85)|SizeofPayload|Payload|CRC16(XModem)@@
+      int i=0;
+      //  The packet format is as follows:
+      //  @@Header(0x06,0x85)|SizeofPayload|Payload|CRC16(XModem)@@
+
       byte eeprom_packet[5];
       eeprom_packet[0] = 6;
       eeprom_packet[1] = 133;
       eeprom_packet[2] = 8;
-      eeprom_packet[3] = value;
-      eeprom_packet[4] = crc.XModemCrc(eeprom_packet, 0, 4);
-      Serial.write(eeprom_packet, 5);
-    
+      eeprom_packet[3] = 130; //value;
+      eeprom_packet[4] = calcrc((char*)eeprom_packet, 4);
+      byte data[] = "61338130";
+      unsigned short value = calcrc((char*)data, 8);
+      unsigned short vvvvv = calcrc((char*)eeprom_packet, 4);
+      Serial.print("crc DEFAULT = 0x");
+      Serial.println(value, HEX);
+
+    //  for (i = 0; i <= 4; i++) {
+    //      Serial.print(eeprom_packet[i]);
+    //
+    //  }
+      Serial.print(eeprom_packet[0]);
+      Serial.print(eeprom_packet[1]);
+      Serial.print(eeprom_packet[2]);
+      Serial.print(eeprom_packet[3]);
+      Serial.print("crc = 0x");
+      Serial.println(vvvvv, HEX);
 
 
-byte i=0;
     for (i = 1; i <= 5; i++) {
     char response[] = "O""K";
       //checking that data transfed and crc16 is correct,
@@ -86,4 +102,26 @@ byte i=0;
     }
     }
   }
+}
+
+//Check routine taken from
+//http://web.mit.edu/6.115/www/miscfiles/amulet/amulet-help/xmodem.htm
+int calcrc(char *ptr, int count)
+{
+    int  crc;
+    char i;
+    crc = 0;
+    while (--count >= 0)
+    {
+        crc = crc ^ (int) *ptr++ << 8;
+        i = 8;
+        do
+        {
+            if (crc & 0x8000)
+                crc = crc << 1 ^ 0x1021;
+            else
+                crc = crc << 1;
+        } while(--i);
+    }
+    return (crc);
 }
